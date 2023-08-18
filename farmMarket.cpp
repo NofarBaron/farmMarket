bbfae0071accc914a929ff244ae1002cf5dae105
Nofar Baron <104790371+NofarBaron@users.noreply.github.com>
2023-08-18 17:02:16 +0300

    Update farmMarket.cpp

--- farmMarket.cpp
+++ farmMarket.cpp
@@ -2,46 +2,6 @@
 #include "Market.h"
 
 int main() {
-
-//    Farm *f[3];
-//    Farm *f0 = new cowFarm();
-//    Farm *f1 = new chickenFarm();
-//    Farm *f2=new sheepFarm();
-//    f[0]=f0,f[1]=f1,f[2]=f2;
-//    f[0]->printFarm();
-//    f[1]->printFarm();
-//    f[2]->printFarm();
-//    f[0]->update_age(4);
-//    f[1]->update_age(2); //chicken die at age of 3
-//    f[2]->update_age(3);
-//
-//    cout << "--after updating--" << endl;
-//    f[0]->printFarm();
-//    f[1]->printFarm();
-//    f[2]->printFarm();
-//
-//    f[0]->products_purchase(f[2]);
-//    f[0]->products_purchase(f[1]);
-//    //f[0]->update_age(1);
-//    cout << "--after purchase--" << endl;
-//    f[0]->printFarm();
-//    f[1]->printFarm();
-//    f[2]->printFarm();
-//
-//    cout << "--after animal purchase--" << endl;
-//    f[0]->animals_purchase();
-//    f[0]->update_age(1);
-//    f[0]->printFarm();
-//    f[1]->printFarm();
-//    f[2]->printFarm();
-//
-//    Market* market = new Market();
-//    market->nextYear();
-//    cout<< "Year:" << market->getYear() <<endl;
-//    market->printMarketFarms();
-//    //market->Trading_day();
-//    //market->printMarketFarms();
-
     Market* market = new Market();
     char gameOption='0';
     while(gameOption!='4')
